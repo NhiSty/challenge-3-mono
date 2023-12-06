badb4d9ef21923ad7e3f8873ff6e76c820a55145
hkanoute <abdelhamidou4@gmail.com>
2023-12-06 14:23:55 +0100

    added User denormilizer to hash password

--- api/tests/Api/GreetingsTest.php
+++ api/tests/Api/GreetingsTest.php
@@ -6,9 +6,58 @@ use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 
 class GreetingsTest extends ApiTestCase
 {
+
+    protected function login($email = 'user@gmail.com', $password = 'test123!')
+    {
+        $client = static::createClient();
+
+       $response =  $client->request(
+            'POST',
+            '/login',
+            [
+                'json' => [
+                    'email' => $email,
+                    'password' => $password,
+                ],
+                "headers" => [
+                    "Content-Type" => "application/ld+json"
+                ]
+            ]
+        );
+
+       var_dump($response->toArray());
+        return $response->toArray()['token'];
+    }
+
+    protected function createAuthenticatedClient($email = 'user@gmail.com', $password = '$2y$13$Fs05Q7OWSMIj6ZwxXitCiu9m5fmqp35aSrMNmlV0xND1rY6DnPWUO')
+    {
+        $client = static::createClient();
+
+       $client->request(
+            'POST',
+            '/users',
+            [
+                'json' => [
+                    "username" => "user",
+                    'password' => $password,
+                    'email' => $email,
+                    'firstName' => 'userFirstName',
+                    'lastName' => 'userLastName',
+                    "age" => 20,
+                ],
+                "headers" => [
+                    "Content-Type" => "application/ld+json"
+                ]
+            ]
+        );
+
+    }
     public function testCreateGreeting(): void
     {
-        static::createClient()->request('POST', '/greetings', [
+      //  $this->createAuthenticatedClient();
+        $client =  $this->login();
+
+        $client->request('POST', '/greetings', [
             'json' => [
                 'name' => 'Kévin',
             ],
