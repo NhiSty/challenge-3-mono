2cde6c11c4f704d1e2a0b8391909a182164b589d
Thomas J <thomas.jallu@gmail.com>
2024-01-30 04:59:11 +0100

    Custom token creation when user login

---
+++ back/api/src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JWTCreatedListener
+{
+    /**
+     * @param JWTCreatedEvent $event
+     *
+     * @return void
+     */
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $payload = $event->getData();
+        $user = $event->getUser();
+
+        $payload['current_user'] = [
+            'roles' => $user->getRoles(),
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'firstName' => $user->getFirstName(),
+            'lastName' => $user->getLastName(),
+        ];
+
+        $event->setData($payload);
+    }
+}
