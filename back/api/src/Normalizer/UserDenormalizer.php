c186d649d48350ee14ec37c0dd779ad8c61ce4bd
hkanoute <abdelhamidou4@gmail.com>
2024-02-12 12:30:50 +0100

    updated denormalizer

--- back/api/src/Normalizer/UserDenormalizer.php
+++ back/api/src/Normalizer/UserDenormalizer.php
@@ -9,6 +9,9 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserDenormalizer implements DenormalizerInterface
 {
@@ -16,11 +19,28 @@ class UserDenormalizer implements DenormalizerInterface
         protected Security $security,
         protected PasswordHasherFactoryInterface $hasher,
         protected ObjectNormalizer $normalizer,
+        protected ValidatorInterface $validator
+
     ) {}
 
     public function denormalize(mixed $data, string $type, string $format = null, array $context = []): mixed
     {
         $user = $this->normalizer->denormalize($data, $type, $format, $context);
+
+        $constraints = new Assert\Collection([
+            'email' => [new Assert\NotBlank(), new Assert\Regex(['pattern' => '/\w+@\w+\.\w+/'])],
+            'plainPassword' => [new Assert\NotBlank()],
+            'firstName' => [new Assert\NotBlank()],
+            'lastName' => [new Assert\NotBlank()],
+            'username' => [new Assert\NotBlank()],
+            'age' => [new Assert\NotBlank()],
+        ]);
+
+        $violations = $this->validator->validate($data, $constraints);
+
+        if (count($violations) > 0) {
+            return new Response('Les données ne sont pas valides.', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
 
         assert($user instanceof User);
 
