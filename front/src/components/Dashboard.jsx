01b0dcf54ba5ddce716d073f8e0d4df404f6dd30
Thomas J <thomas.jallu@gmail.com>
2024-02-16 11:16:55 +0100

    Push last change

--- front/src/components/Dashboard.jsx
+++ front/src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import useToken from "@/hooks/useToken";
+import useToken, { ROLES } from "@/hooks/useToken";
 import ManagerDashboardPage from "@routes/ManagerDashboardPage";
 import AdminDashboardPage from "@routes/AdminDashboardPage";
 import { Navigate } from "react-router-dom";
@@ -6,11 +6,11 @@ import { Navigate } from "react-router-dom";
 export default function Dashboard() {
   const { roles } = useToken();
 
-  if (roles.includes("ROLE_MANAGER")) {
+  if (roles.includes(ROLES.MANAGER)) {
     return <ManagerDashboardPage />;
   }
 
-  if (roles.includes("ROLE_ADMIN")) {
+  if (roles.includes(ROLES.ADMIN)) {
     return <AdminDashboardPage />;
   }
 
